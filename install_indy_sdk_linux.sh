a707ff5f9d4d7a96d2733465ba931a6bf910c794
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-10-11 22:23:01 +0200

    added linux indy sdk intall script

---
+++ install_indy_sdk_linux.sh
@@ -0,0 +1,5 @@
+sudo apt-get install ca-certificates -y
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CE7709D068DB5E88
+sudo add-apt-repository "deb https://repo.sovrin.org/sdk/deb bionic master"
+sudo apt-get update
+sudo apt-get install -y libindy indy-cli
