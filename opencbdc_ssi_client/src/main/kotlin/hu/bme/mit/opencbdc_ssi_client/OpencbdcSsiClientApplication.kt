08ef59f0f6bacf9344f6c056aa0e00727f5c5764
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-10-13 13:36:20 +0200

    Init java project

---
+++ opencbdc_ssi_client/src/main/kotlin/hu/bme/mit/opencbdc_ssi_client/OpencbdcSsiClientApplication.kt
@@ -0,0 +1,11 @@
+package hu.bme.mit.opencbdc_ssi_client
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpencbdcSsiClientApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpencbdcSsiClientApplication>(*args)
+}
---
+++ opencbdc_ssi_client/src/test/kotlin/hu/bme/mit/opencbdc_ssi_client/OpencbdcSsiClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.bme.mit.opencbdc_ssi_client
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpencbdcSsiClientApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
