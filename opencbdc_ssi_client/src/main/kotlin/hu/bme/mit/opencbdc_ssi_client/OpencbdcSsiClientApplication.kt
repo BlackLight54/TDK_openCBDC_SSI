26f3e7a481b855684a0613a44d376eb335504023
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-10-13 20:25:07 +0200

    Spring shell working

--- opencbdc_ssi_client/src/main/kotlin/hu/bme/mit/opencbdc_ssi_client/OpencbdcSsiClientApplication.kt
+++ opencbdc_ssi_client/src/main/kotlin/hu/bme/mit/opencbdc_ssi_client/OpencbdcSsiClientApplication.kt
@@ -1,11 +1,26 @@
 package hu.bme.mit.opencbdc_ssi_client
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class OpencbdcSsiClientApplication
 
+@SpringBootApplication
+class OpencbdcSsiClientApplication /*: CommandLineRunner*/{
+    /*
+    private val LOG: Logger = LoggerFactory
+        .getLogger(OpencbdcSsiClientApplication::class.java)
+
+    @Override
+    override fun run(vararg args: String?) {
+        LOG.info("EXECUTING : command line runner")
+        for (i in args.indices) {
+            LOG.info("args[{}]: {}", i, args[i])
+        }
+    }*/
+}
 fun main(args: Array<String>) {
     runApplication<OpencbdcSsiClientApplication>(*args)
 }
