4a873719d4e4d3e40a3522aeb17202c22f0f57e6
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-10-13 11:44:14 +0200

    cleaned up scripts

--- install_indy_sdk_linux.sh
+++ install_indy_sdk_linux.sh
@@ -1,5 +1,5 @@
 sudo apt-get install ca-certificates -y
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CE7709D068DB5E88
-sudo add-apt-repository "deb https://repo.sovrin.org/sdk/deb bionic master"
+sudo add-apt-repository "deb https://repo.sovrin.org/sdk/deb xenial master"
 sudo apt-get update
 sudo apt-get install -y libindy indy-cli
--- run_von-network_ledger_browser.sh
+++
@@ -1,1 +0,0 @@
-GENESIS_FILE=/path/to/local-genesis.txt PORT=9000 REGISTER_NEW_DIDS=true python -m server.server
