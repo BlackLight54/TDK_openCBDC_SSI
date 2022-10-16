30c266d1e2e3d22d0dedb7c2b4d8df132881ef7d
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-10-16 18:37:53 +0200

    Call API started

--- opencbdc_ssi_client/src/main/kotlin/hu/bme/mit/opencbdc_ssi_client/Endpoints.kt
+++ opencbdc_ssi_client/src/main/kotlin/hu/bme/mit/opencbdc_ssi_client/Endpoints.kt
@@ -1,8 +1,17 @@
 package hu.bme.mit.opencbdc_ssi_client
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-@Req
-class RestController {
+
+@RestController
+class Endpoints {
+
+        @GetMapping("/onboard")
+        public fun index(): String {
+            return "onboard Alice"
+        }
+
 }
